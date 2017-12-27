2a17bf9b72bef692040b9d5a51428135b14069fd
lotemh <lotemhiki@gmail.com>
2017-12-27 10:47:33 +0200

    added init function

--- js/game.js
+++ js/game.js
@@ -1,4 +1,17 @@
+var ticTacToe = {};
 
+ticTacToe.init = function () {
+    ticTacToe.currentPlayer = "red";
+    ticTacToe.playerIndicator = $(".current-player-indicator");
+    ticTacToe.playerIndicator.addClass(ticTacToe.currentPlayer);
+    ticTacToe.boxes = $(".box");
+    ticTacToe.boxes.on("click", ticTacToe.boxClicked);
+};
 
+ticTacToe.boxClicked = function (){
+    $(this).addClass(ticTacToe.currentPlayer);
+};
 
-
+$(document).ready(function () {
+    ticTacToe.init();
+});
